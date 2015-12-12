f8093d9f38bf0835ec3c2274c61afb824fcb7e64
Vic Lau <xingyulliiuu@gmail.com>
2015-12-12 16:19:04 +0800

    example of selecting all documents

--- elasticsearch/src/main/kotlin/org/swordess/toy/kotlin/elasticsearch/IndexRepository.kt
+++ elasticsearch/src/main/kotlin/org/swordess/toy/kotlin/elasticsearch/IndexRepository.kt
@@ -6,6 +6,7 @@ import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.common.xcontent.XContentBuilder
 import org.elasticsearch.common.xcontent.XContentFactory
+import org.elasticsearch.search.SearchHit
 import java.net.InetAddress
 
 class IndexRepository(val config: ClientConfig) {
@@ -32,6 +33,20 @@ class IndexRepository(val config: ClientConfig) {
         println("mapping setting updated")
     }
 
+    fun forEachExisted(docType: String, vararg fields: String, handler: (SearchHit) -> Unit) {
+        val searchRequestBuilder = client.prepareSearch(config.indexName).setTypes(docType).setFetchSource(false)
+        if (fields.isEmpty()) {
+            searchRequestBuilder.setNoFields()
+        } else {
+            fields.forEach { searchRequestBuilder.addField(it) }
+        }
+
+        val response = searchRequestBuilder.execute().actionGet()
+        for (hit in response.hits) {
+            handler(hit)
+        }
+    }
+
     companion object {
 
         val MAPPING_SETTING: XContentBuilder = XContentFactory.jsonBuilder().prettyPrint()
@@ -48,4 +63,20 @@ class IndexRepository(val config: ClientConfig) {
 
     }
 
+}
+
+fun main(args: Array<String>) {
+    // demoForEachExisted()
+}
+
+private fun demoForEachExisted() {
+    val clusterName = "yourClusterName"
+    val indexName = "yourIndexName"
+
+    val repository = IndexRepository(ClientConfig("localhost", 9300, clusterName, indexName))
+    repository.forEachExisted("FeeDocument", "feeId", "feeType") {
+        val feeIdValue = it.field("feeId").value<String>()
+        val feeTypeValue = it.field("feeType").value<String>()
+        println("FeeDocument(id=${it.id},feeId=$feeIdValue,feeType=$feeTypeValue)")
+    }
 }
