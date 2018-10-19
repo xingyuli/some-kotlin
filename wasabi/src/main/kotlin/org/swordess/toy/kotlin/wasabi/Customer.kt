c7965679729178cce7903b76acd1b905adea5f6e
Vic Lau <xingyu.liu@mobisist.com>
2018-10-19 15:27:52 +0800

    cleanup

--- wasabi/src/main/kotlin/org/swordess/toy/kotlin/wasabi/Customer.kt
+++
@@ -1,3 +0,0 @@
-package org.swordess.toy.kotlin.wasabi
-
-class Customer(val name: String, val email: String)
--- wasabi/src/main/kotlin/org/swordess/toy/kotlin/wasabi/HelloWorldDemo.kt
+++
@@ -1,9 +0,0 @@
-package org.swordess.toy.kotlin.wasabi
-
-import org.wasabi.app.AppServer
-
-fun main(args: Array<String>) {
-    var server = AppServer()
-    server.get("/", { /* response.send("Hello World!") */ })
-    server.start()
-}
